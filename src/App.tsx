0e8f6caf94fd583211eb259bff9b8df54835cd96
blondeljerome <blondel.jerome@gmail.com>
2020-02-19 12:26:54 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, {FunctionComponent} from 'react';
+import PokemonList from "./pages/pokemon-list";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import PokemonsDetail from "./pages/pokemon-detail";
+import PageNotFound from "./pages/page-not-found";
+import PokemonEdit from "./pages/pokemon-edit";
+import PokemonAdd from "./pages/pokemon-add";
+
+const App: FunctionComponent = () => {
+
+    return (
+        <Router>
+            <div>
+                {/** Barre de navigation **/}
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <Link to="/" className="brand-logo center">Pokédex</Link>
+                    </div>
+                </nav>
+                {/** Gestion des routes **/}
+                <Switch>
+                    <Route exact path="/" component={PokemonList}/>
+                    <Route exact path="/pokemons" component={PokemonList}/>
+                    <Route exact path="/pokemons/add" component={PokemonAdd}/>
+                    <Route exact path="/pokemons/edit/:id" component={PokemonEdit}/>
+                    <Route path="/pokemons/:id" component={PokemonsDetail}/>
+                    <Route component={PageNotFound}/>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
